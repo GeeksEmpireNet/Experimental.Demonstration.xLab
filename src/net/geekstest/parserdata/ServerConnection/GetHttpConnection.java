02df6f93cfbb4b63fb51d68c4b62113b0aef9041
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-01-20 12:15:09 -0900

    Initial

--- src/net/geekstest/parserdata/ServerConnection/GetHttpConnection.java
+++ src/net/geekstest/parserdata/ServerConnection/GetHttpConnection.java
@@ -1,6 +1,8 @@
 package net.geekstest.parserdata.ServerConnection;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
@@ -9,19 +11,29 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import net.geekstest.parserdata.JSON.JSONObject;
+import net.geekstest.parserdata.JSON.JSONParser;
+
 public class GetHttpConnection {
 	public static void main(String[] args) {		
 		// The following line initializes the HttpGet Object with the URL in order to send a request
-        HttpGet get = new HttpGet("http://api.currencylayer.com/live?access_key=3057d94042a99324b134799aa7c252f9");
-
+        HttpGet httpGet = new HttpGet("http://api.currencylayer.com/live?access_key=3057d94042a99324b134799aa7c252f9");
+        
         try {
-            CloseableHttpResponse response =  HttpClients.createDefault().execute(get);
+            CloseableHttpResponse response =  HttpClients.createDefault().execute(httpGet);
             HttpEntity entity = response.getEntity();
 
-            System.out.println(EntityUtils.toString(entity));
-            //EntityUtils.toString(entity);
-
-            System.out.println("Live Currency Exchange Rates");
+            String fullJsonResponse = EntityUtils.toString(entity);
+            System.out.println(fullJsonResponse);
+            
+            JSONParser jsonParser = new JSONParser();
+            JSONObject fullJson = (JSONObject) jsonParser.parse(fullJsonResponse);
+            System.out.println(fullJson.get("quotes"));
+            
+            HashMap<String, Long> rates = (HashMap<String, Long>) fullJson.get("quotes");
+            rates.keySet().forEach( key -> {
+            	System.out.println(key + ": " + rates.get(key));
+            });
 
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
@@ -32,7 +44,10 @@ public class GetHttpConnection {
         } catch (ParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        } catch (net.geekstest.parserdata.JSON.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
